11cf71406a95f02b26fdd8271a974d36f7e8a033
FRCCallister <105382419+FRCCallister@users.noreply.github.com>
2022-05-25 15:00:06 +0300

    Navbar Yapılıyor
    Linklere devam edilecek.

--- includes/navbar.inc.php
+++ includes/navbar.inc.php
@@ -1,3 +1,24 @@
+<?php
+
+    $oneHeader = "HAKKIMIZDA";
+    $oneOne = "Hakkımmıda";
+    $oneTwo = "Takımımız";
+
+    $twoHeader = "FRC";
+    $twoOne = "FRC Nedir?";
+    $twoTwo = "";
+
+    $threeHeader = "PROJELER";
+    $threeOne = "Sosyal Sorumluluk";
+    $threeTwo = "Genel Projeler";
+    
+    $fourHeader = "SPONSORLUK";
+    $fourOne = "Sponsorluk";
+    $fourTwo = "Sponsorluğun Katkıları";
+
+
+?>
+
 <header class="navbar-header">
 
     <section class="navbar-header-section-up">
@@ -5,7 +26,7 @@
         <span class="material-icons">
             email
         </span>
-        <button onclick="navigator.clipboard.writeText('eragonfrc@gmail.com');" style="background: none; border: none; color: #F9ED69; font-size: medium; cursor: pointer;">frceragon@gmail.com</button>
+        <button onclick="navigator.clipboard.writeText('eragonfrc@gmail.com');" style="background: none; border: none; color: #F25D9C; font-size: medium; cursor: pointer;">frceragon@gmail.com</button>
         <a class="navbar-up-responsive-willberemoved" href="#" style="margin-left: 20px;">Instagram</a>
         <a class="navbar-up-responsive-willberemoved" href="#">YouTube</a>
         <a class="navbar-up-responsive-willberemoved" href="#">Twitter</a>
@@ -37,43 +58,36 @@
             </a>
 
             <div id="navbar-dropdown" class="navbar-dropdown navbar-content-extra-margin">
-                <button class="navbar-dropdown-button">FRC</button>
+                <button class="navbar-dropdown-button"><?php echo $oneHeader; ?></button>
                 <div class="navbar-dropdown-content">
-                    <a href="eragonfrc.php">EragonFRC</a>
-                    <a href="eragonfrchakkinda.php">Hakkımızda</a>
-                    <a href="eragonfrctakim.php">Takımımız</a>
+                    <a href="eragonfrchakkinda.php"><?php echo $oneOne ?></a>
+                    <a href="eragonfrctakim.php"><?php echo $oneTwo; ?></a>
                 </div>
             </div>
 
             <div id="navbar-dropdown" class="navbar-dropdown navbar-content-extra-margin">
-                <button class="navbar-dropdown-button">TÜBİTAK</button>
+                <button class="navbar-dropdown-button"><?php echo $twoHeader; ?></button>
                 <div class="navbar-dropdown-content">
-                    <a href="#">Çok Yakında!</a>
+                    <a href="frcnedir.php"><?php echo $twoOne; ?></a>
                 </div>
             </div>
 
             <div id="navbar-dropdown" class="navbar-dropdown navbar-content-extra-margin">
-                <button class="navbar-dropdown-button">TEKNOFEST</button>
+                <button class="navbar-dropdown-button"><?php echo $threeHeader; ?></button>
                 <div class="navbar-dropdown-content">
-                <a href="#">Çok Yakında!</a>
+                    <a href="#"><?php echo $threeOne; ?></a>
+                    <a href="#"><?php echo $threeTwo; ?></a>
                 </div>
             </div>
 
             <div id="navbar-dropdown" class="navbar-dropdown navbar-content-extra-margin">
-                <button class="navbar-dropdown-button">İNOVASYON</button>
+                <button class="navbar-dropdown-button"><?php echo $fourHeader; ?></button>
                 <div class="navbar-dropdown-content">
                     <!-- <a href="inovasyon.php">İnovasyon</a> -->
-                    <a href="inovasyon.php">İnovasyon</a>
+                    <a href="inovasyon.php"><?php echo $fourOne; ?></a>
+                    <a href="inovasyon.php"><?php echo $fourTwo; ?></a>
                 </div>
             </div>
-
-            <a href="sponsorluk.php" style="text-decoration: none;" class="navbar-dropdown-button navbar-content-extra-margin navbar-up-responsive-willberemoved">
-                SPONSORLUK
-            </a>
-
-            <a href="medya.php" style="text-decoration: none;" class="navbar-dropdown-button navbar-content-extra-margin navbar-up-responsive-willberemoved">
-                MEDYA
-            </a>
 
         </div>
 
@@ -102,24 +116,26 @@
 
         <div class="responsive-open-menu-main">
 
-            <a class="responsive-open-menu-title" href="#">FRC</a>
+            <a class="responsive-open-menu-title" href="#"><?php echo $oneHeader; ?></a>
 
-            <a class="responsive-open-menu-title-open" href="eragonfrc.php">EragonFRC</a>
-            <a class="responsive-open-menu-title-open" href="eragonfrchakkinda.php">Hakkımızda</a>
-            <a class="responsive-open-menu-title-open" href="eragonfrctakim.php">Takımımız</a>
+            <a class="responsive-open-menu-title-open" href="eragonfrc.php"><?php echo $oneOne ?></a>
+            <a class="responsive-open-menu-title-open" href="eragonfrchakkinda.php"><?php echo $oneTwo; ?></a>
 
         </div>
 
         <div class="responsive-open-menu-main">
 
-            <a class="responsive-open-menu-title" href="#">TÜBİTAK</a>
+            <a class="responsive-open-menu-title" href="#"><?php echo $twoHeader; ?></a>
+
+            <a class="responsive-open-menu-title-open" href="frcnedir.php"><?php echo $twoOne; ?></a>
             
         </div>
 
         <div class="responsive-open-menu-main">
 
-            <a class="responsive-open-menu-title" href="#">TEKNOFEST</a>
+            <a class="responsive-open-menu-title" href="#"><?php echo $threeHeader; ?></a>
 
+            <a class="responsive-open-menu-title-open" href="frcnedir.php"><?php echo $threeOne; ?></a>
         </div>
 
         <div class="responsive-open-menu-main">
